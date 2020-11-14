951315d10758cd260c906ad0216ea441e10a5cf9
Bloodyaugust <greysonrichey@gmail.com>
2020-11-14 04:27:41 -0700

    Add test UI for joining games

---
+++ scripts/controllers/LobbyController.gd
@@ -0,0 +1,41 @@
+extends CanvasLayer
+
+onready var _host_button: Button = $"./MarginContainer/CenterContainer/VBoxContainer/Host"
+onready var _ip_address_input: LineEdit = $"./MarginContainer/CenterContainer/VBoxContainer/IPAddress"
+onready var _join_button: Button = $"./MarginContainer/CenterContainer/VBoxContainer/Join"
+onready var _play_button: Button = $"./MarginContainer/CenterContainer/VBoxContainer/Play"
+
+onready var _network_controller: Node = $"../".find_node("NetworkController")
+
+
+func _on_host_button_pressed():
+	_network_controller.create_server(31400)
+	store.dispatch(actions.client_set_state(ClientConstants.GAME))
+	store.emit_signal("game_initializing")
+
+
+func _on_join_button_pressed():
+	_network_controller.create_client(_ip_address_input.text, 31400)
+	store.dispatch(actions.client_set_state(ClientConstants.GAME))
+	store.emit_signal("game_initializing")
+
+
+func _on_play_button_pressed():
+	store.dispatch(actions.client_set_state(ClientConstants.GAME))
+	store.emit_signal("game_initializing")
+
+
+func _on_store_updated(name, state):
+	match name:
+		"client":
+			if state["state"] == ClientConstants.GAME:
+				offset.y = 2000
+			else:
+				offset.y = 0
+
+
+# func _ready():
+# 	_host_button.connect("pressed", self, "_on_host_button_pressed")
+# 	_join_button.connect("pressed", self, "_on_join_button_pressed")
+# 	_play_button.connect("pressed", self, "_on_play_button_pressed")
+# 	store.subscribe(self, "_on_store_updated")
