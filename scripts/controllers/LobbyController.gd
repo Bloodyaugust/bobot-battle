6259aa67e835ffc9f1b7ab9e89db5b6c36eec994
Bloodyaugust <greysonrichey@gmail.com>
2020-11-15 13:25:27 -0700

    Connect lobby to local lobby server

--- scripts/controllers/LobbyController.gd
+++ scripts/controllers/LobbyController.gd
@@ -4,6 +4,8 @@ export var lobby_component: PackedScene
 
 onready var _actions_container: Node = find_node("Actions")
 onready var _lobbies_container: Node = find_node("Lobbies")
+onready var _lobby_timer: Timer = $"./Get Lobbies Timer"
+onready var _lobby_updater: HTTPRequest = $"./Lobby Updater"
 onready var _network_controller: Node = $"../".find_node("NetworkController")
 
 onready var _direct_join_button: Button = _actions_container.get_node("./Direct Join")
@@ -11,7 +13,7 @@ onready var _join_button: Button = _actions_container.get_node("./Join")
 onready var _ip_address_input: LineEdit = _actions_container.get_node("./IPAddress")
 onready var _main_menu_button: Button = _actions_container.get_node("./Main Menu")
 
-var _lobby_data: Array = [{"host": "127.0.0.1", "name": "Test Map Small N00BZ ONLY"}, {"host": "127.0.0.2", "name": "Test Map Small PR0Z ONLY"}, {"host": "127.0.0.3", "name": "Test Map Small ALL WELCOME"}]
+var _lobby_data: Array = []
 var _selected_lobby
 
 
@@ -31,7 +33,7 @@ func _on_lobby_component_button_pressed(lobby):
 	for _lobby_component in _lobbies_container.get_children():
 		var _lobby_component_button = _lobby_component.find_node("Button")
 
-		if !(lobby.host in _lobby_component.find_node("Lobby Info").text):
+		if !(lobby.name in _lobby_component.find_node("Lobby Info").text):
 			_lobby_component_button.pressed = false
 		else:
 			if _lobby_component_button.pressed:
@@ -43,6 +45,8 @@ func _on_lobby_component_button_pressed(lobby):
 
 
 func _on_lobby_data_updated():
+	var _selected_lobby_exists: bool = false
+
 	GDUtil.queue_free_children(_lobbies_container)
 
 	for _lobby in _lobby_data:
@@ -51,7 +55,27 @@ func _on_lobby_data_updated():
 		_new_lobby_component.find_node("Lobby Info").text = "{host} - {name}".format(_lobby)
 		_new_lobby_component.find_node("Button").connect("pressed", self, "_on_lobby_component_button_pressed", [_lobby])
 
+		if _selected_lobby && _selected_lobby.name == _lobby.name:
+			_new_lobby_component.find_node("Button").pressed = true
+			_selected_lobby_exists = true
+
 		_lobbies_container.add_child(_new_lobby_component)
+
+	if !_selected_lobby_exists:
+		_selected_lobby = null
+
+	_join_button.disabled = _selected_lobby == null
+
+
+func _on_lobby_timer_timeout():
+	_lobby_updater.request("http://localhost:3000/")
+
+
+func _on_lobby_updater_request_completed(result, response_code, headers, body):
+	var _json_response = JSON.parse(body.get_string_from_utf8()).result
+
+	_lobby_data = _json_response
+	_on_lobby_data_updated()
 
 
 func _on_main_menu_button_pressed():
@@ -63,13 +87,17 @@ func _on_store_updated(name, state):
 		"client":
 			if state["state"] == ClientConstants.LOBBY:
 				offset.y = 0
-				_on_lobby_data_updated()
+				_on_lobby_timer_timeout()
+				_lobby_timer.start()
 			else:
 				offset.y = 2000
+				_lobby_timer.stop()
 
 
 func _ready():
 	_direct_join_button.connect("pressed", self, "_on_direct_join_button_pressed")
 	_join_button.connect("pressed", self, "_on_join_button_pressed")
+	_lobby_timer.connect("timeout", self, "_on_lobby_timer_timeout")
+	_lobby_updater.connect("request_completed", self, "_on_lobby_updater_request_completed")
 	_main_menu_button.connect("pressed", self, "_on_main_menu_button_pressed")
 	store.subscribe(self, "_on_store_updated")
